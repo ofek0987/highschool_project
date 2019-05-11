faee62fa565a1950fea737d6d61956e27a1e0813
ofek <ofek0987@gmail.com>
2019-05-11 19:59:32 +0300

    all working no dialog

--- app/src/main/java/com/yaniv/student/project2/reminderService.java
+++ app/src/main/java/com/yaniv/student/project2/reminderService.java
@@ -1,14 +1,36 @@
 package com.yaniv.student.project2;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+
 
 public class reminderService extends IntentService {
 
     public reminderService() {
         super("reminderService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.trump)
+                .setContentTitle("This bloody putin")
+                .setContentText("let's kill him");
+        ;
+
+        startForeground(1 , builder.build());
+    }
+
+    @Override
+    public int onStartCommand( Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+
+        return START_STICKY;
     }
 
     @Override
@@ -20,12 +42,12 @@ public class reminderService extends IntentService {
         NotificationManager notificationManager = (NotificationManager) getSystemService(this.NOTIFICATION_SERVICE);
 
             try {
-                Thread.sleep(10800000);
+             Thread.sleep(10000);                 //10800000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            notificationManager.notify(1, builder.build());
+            notificationManager.notify(2, builder.build());
 
 
     }}
