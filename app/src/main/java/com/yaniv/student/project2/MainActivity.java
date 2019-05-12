71ed48a1130ff4b99a826b5e270d39d64b8627f9
ofek <ofek0987@gmail.com>
2019-05-12 07:03:51 +0300

    all working no dialog

--- app/src/main/java/com/yaniv/student/project2/MainActivity.java
+++ app/src/main/java/com/yaniv/student/project2/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -97,5 +98,12 @@ public class MainActivity extends AppCompatActivity  {
         startActivity(termsIntent);
         finish();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
